9d3471f4b362c163128f5005e8549a2ff88bfe2d
Alejandro Arellano <janoare@gmail.com>
2020-07-20 19:34:48 -0400

    Fix test, add heroku integration

--- src/test/java/unit/wallmartbackend/WallmartBackendApplicationTests.java
+++ src/test/java/unit/wallmartbackend/WallmartBackendApplicationTests.java
@@ -5,7 +5,8 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 
-@SpringBootTest(classes = WallmartBackendApplication.class)
+@SpringBootTest(classes = WallmartBackendApplication.class,
+		webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class WallmartBackendApplicationTests {
 
 	@Test
